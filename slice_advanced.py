7c29e4f68ce2896263298b96fb7f7f558f57b2bc
Senaf123 <fssena@mail.austral.edu.ar>
2025-08-25 12:33:59 -0300

    slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,6 +2,5 @@ def slice_advanced():
     text = input("ingresar texto:")
     print(text[4::2])
     # Código a implementar utilizando input.
-
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
