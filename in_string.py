96c99f218c014069ed0fdf670bbddf9157dcb6df
Senaf123 <fssena@mail.austral.edu.ar>
2025-08-25 12:03:28 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,4 +1,10 @@
 def check_vowels():
+    nombre = input("ingresar nombre:").lower()
+    print(f"Contiene a: {"a" in nombre}")
+    print(f"Contiene e: {"e" in nombre}")
+    print(f"Contiene i: {"i" in nombre}")
+    print(f"Contiene o: {"o" in nombre}")
+    print(f"Contiene u: {"u" in nombre}")
     # Código a implementar utilizando input.
 
 
---
+++ tp3-Senaf123-main/slice_advanced.py
@@ -0,0 +1,7 @@
+def slice_advanced():
+    text = input("ingresar texto:")
+    print(text[4::2])
+    # Código a implementar utilizando input.
+
+# Para verificar este ejercicio ejecutar el comando
+# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
