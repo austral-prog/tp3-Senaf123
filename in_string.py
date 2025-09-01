68ad03a1593b159d19b77dd4bbb0810f3395f376
Senaf123 <fssena@mail.austral.edu.ar>
2025-09-01 11:21:31 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,7 +1,7 @@
 def check_vowels():
     nombre = input("ingresar nombre:").lower()
-    print(f"Contiene a: {"a" in nombre}")
-    print(f"Contiene e: {"e" in nombre}")
-    print(f"Contiene i: {"i" in nombre}")
-    print(f"Contiene o: {"o" in nombre}")
-    print(f"Contiene u: {"u" in nombre}")
+    print(f"Contiene a: {'a' in nombre}")
+    print(f"Contiene e: {'e' in nombre}")
+    print(f"Contiene i: {'i' in nombre}")
+    print(f"Contiene o: {'o' in nombre}")
+    print(f"Contiene u: {'u' in nombre}")
